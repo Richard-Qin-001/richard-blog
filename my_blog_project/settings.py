1527a2908c4d2456def719fe5701f0ea2d2ec179
Richard-Qin-001 <richard.qin.001@gmail.com>
2025-12-20 02:39:08 +0800

    Fix remote server configuration issue

--- my_blog_project/settings.py
+++ my_blog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['richardqin.pythonanywhere.com', '127.0.0.1']
 
